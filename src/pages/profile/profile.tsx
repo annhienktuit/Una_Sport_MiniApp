518ce82a6338ccdc72d54ffc548678747c078506
nhiennha <nhiennha@vng.com.vn>
2023-08-12 04:44:19 +0700

    update profile

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,18 +1,21 @@
-import React, { startTransition } from "react";
+import React, { useState, useEffect } from "react";
 import { useRecoilValue } from "recoil";
+import { getUserInfo } from "zmp-sdk/apis";
 import { Avatar, Page, Text } from "zmp-ui";
 import { userState } from "../../state";
 
 function Welcome() {
-  const user = useRecoilValue(userState);
+  // const user = useRecoilValue(userState);
   return (
     <div className="flex flex-col items-center">
-      <Avatar size={48} className="shadow align-middle mb-2" src={user.avatar}>
+      <Avatar
+        size={48}
+        className="shadow align-middle mb-2"
+        src={"https://h5.zdn.vn/static/images/avatar.png"}
+      >
         Avatar
       </Avatar>
-      <Text className="font-bold text-4xl mt-2">
-        {user.name ? user.name : "..."}
-      </Text>
+      <h1 className="text-2xl font-bold text-gray-900">Nhien Nguyen</h1>
     </div>
   );
 }
