ac9b77fe696feed2bbbaacfadfba3b91e6c60498
nhiennha <nhiennha@vng.com.vn>
2023-08-12 17:01:53 +0700

    mock calendar data

--- src/pages/calendar/booking-calendar-list.tsx
+++ src/pages/calendar/booking-calendar-list.tsx
@@ -1,29 +1,40 @@
 import React, { FC, useEffect, useState } from "react";
-import { Text } from "zmp-ui";
+import { localizeString } from "../../assets/string";
+import { Button, Text } from "zmp-ui";
 import { Section } from "../../components/ui/section";
-import { ref, get, child } from "@firebase/database";
-import { Booking } from "../../models/models";
+import { RatingStar } from "../../components/ui/ratingStar";
+import { ref, getDatabase, get, child } from "@firebase/database";
+import { SportCenter } from "../../models/models";
 import { realTimeDB } from "../../components/firebase/firebase";
+import { generateRandomNumber } from "../../utils/ultils";
 import { useNavigate } from "react-router";
-import { bookingState } from "../../state";
+import { slectedSportCenterState } from "../../state";
 
 import { useRecoilState } from "recoil";
 import { dateState } from "../../state";
+import { filterAvailableCourts } from "../../models/functions";
 
 export const BookingCalendarListContent: FC = () => {
   const navigate = useNavigate();
-  const [bookings, setBookings] = useState<Booking[]>([]);
-  const [booking, setBooking] = useRecoilState(bookingState);
+  const [sportCenters, setSportCenters] = useState<SportCenter[]>([]);
+  const [sportCenter, setSportCenter] = useRecoilState(slectedSportCenterState);
+
   const [date, setDate] = useRecoilState(dateState);
 
   useEffect(() => {
     const dbRef = ref(realTimeDB);
-    get(child(dbRef, "Booking"))
+    get(child(dbRef, "SportCenter"))
       .then((snapshot) => {
         if (snapshot.exists()) {
-          const bookingData = snapshot.val();
-          const bookings: Booking[] = Object.values(bookingData);
-          setBookings(bookings);
+          const sportCenterData = snapshot.val();
+          const sportCenterArray: SportCenter[] =
+            Object.values(sportCenterData);
+          console.log("get List sport!");
+          const filterByDateArr = filterAvailableCourts(
+            sportCenterArray,
+            date.getDate()
+          );
+          setSportCenters(filterByDateArr);
         } else {
           console.log("No data available");
         }
@@ -33,16 +44,19 @@ export const BookingCalendarListContent: FC = () => {
       });
   }, [date]);
 
-  const handleClick = (data: Booking) => {
+  const handleClick = (data: SportCenter) => {
     console.log("Clicked data:", data);
-    setBooking(data);
-    navigate("/detail-info");
+    setSportCenter(data);
+    navigate("/detailSportCenter");
   };
 
+  const randomCount = generateRandomNumber();
+  const availabilityText = `Còn ${randomCount} sân`;
+
   return (
-    <Section title={"Lịch trình booking sắp tới của bạn"}>
+    <Section title="Quản lý booking của bạn">
       <div className="grid grid-cols-1 gap-4">
-        {bookings.map((data, key) => (
+        {sportCenters.map((data, key) => (
           <div
             key={key}
             onClick={() => handleClick(data)}
@@ -55,23 +69,25 @@ export const BookingCalendarListContent: FC = () => {
                   size={"small"}
                   className="text-text01 line-clamp-2 max-lines pr-2"
                 >
-                  Chủ sân {data.host.zaUserInfo.name}
+                  {data.name}
                 </Text>
               </div>
-
               <Text
                 size={"xxSmall"}
                 className="text-text03 line-clamp-1 max-lines"
               >
-                Sân: {data.yard}
+                {data.address}
               </Text>
 
+              <Text size={"xxSmall"} className="text-text03">
+                {`Còn ${generateRandomNumber()} sân`}
+              </Text>
               <Text size={"xSmall"} className="text-text01"></Text>
             </div>
             <div className="h-24 aspect-square relative">
               <img
                 loading="lazy"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Stamford_Bridge_Clear_Skies.JPG/1200px-Stamford_Bridge_Clear_Skies.JPG"
+                src={data.image[0]}
                 className="absolute left-0 right-0 top-0 bottom-0 w-full h-full object-cover object-center rounded-lg bg-skeleton"
               />
             </div>
